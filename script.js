60c7359e4a3644d2ea45cbe76da078edb38d76bb
Ricardo Silva <devmonskvll@gmail.com>
2022-09-17 16:17:49 +0100

    Simplify data input whenever change is needed

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
-
 function calculateTime() {
-    const timer = document.getElementById("timer");
-    const lastChallengeDate = Date.parse('08 Sep 2022 15: 00: 00 GMT');
+    const lastChallengeDay = "08";
+    const lastChallengeMonth = "Sep";
+    const lastChallengeYear = "2022";
+    const parsedLastChallengeDate = Date.parse(`${lastChallengeDay} ${lastChallengeMonth} ${lastChallengeYear} 15: 00: 00 GMT`);
     const currentDate = Date.now();
-    const ellapsedTime = currentDate - lastChallengeDate;
+    const ellapsedTime = currentDate - parsedLastChallengeDate;
     const daysInMilliseconds = ellapsedTime / 1000 / 60 / 60 / 24;
 
+    const timer = document.getElementById("timer");
     timer.textContent = `${Math.floor(daysInMilliseconds)} days`;
 }
 
